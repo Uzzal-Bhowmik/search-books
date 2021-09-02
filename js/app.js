f89bc5238ca8f2d2c5889ca4ba819e1f84fe4c1e
Uzzal-Bhowmik <ubcreativity111@gmail.com>
2021-09-02 12:00:09 +0600

    default image added and books counter bug fixed

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ document.getElementById('search-btn').addEventListener('click', function () {
 
 
         // displaying card loader 
-        for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < 6; i++) {
             const cardLoaderDiv = document.createElement('div');
             cardLoaderDiv.innerHTML = `
                 <div class="bg-white p-4 rounded-md">
@@ -66,39 +66,53 @@ const displayBookCards = (booksInfo) => {
     else {
 
         bookCardsContainer.textContent = '';
+        nothingFound.style.display = 'none';
+        searchBoxEmpty.style.display = 'none';
+
 
         // updating books counter 
         booksCounter.textContent = '';
         booksCounter.innerHTML = `
-            <pre class="text-1xl text-gray-600 mt-0 font-semibold">Books Found: ${booksInfo.numFound} &Tab; Books Shown: 20</pre>
+            <pre class="text-1xl text-gray-600 mt-0 font-semibold">Books Found: ${booksInfo.numFound} &Tab; Books Shown: ${booksInfo.numFound > 20 ? "20" : booksInfo.numFound}</pre>
         `
 
         // updating book cards container
         const books = booksInfo.docs;
         books.forEach(book => {
 
+            // setting image url to load 
+            var url;
+            if (book.cover_i === undefined) {
+                url = "images/NoCover.png"
+            }
+            else {
+                url = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+            }
+
+
             const bookDiv = document.createElement('div');
             bookDiv.innerHTML = `
-            <div
-                class="container mx-auto max-w-sm bg-white rounded-xl shadow-lg hover:scale-100 hover:shadow-2xl transform transition-all duration-500 h-full">
+                <div
+                    class="container mx-auto max-w-sm bg-white rounded-xl shadow-lg hover:scale-100 hover:shadow-2xl transform transition-all duration-500 h-full">
 
-                <div class="border rounded-xl rounded-b-none w-full">
-                    <img id='book-images' src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg"
-                        alt="" class='mx-auto rounded-xl rounded-b-none'>
-                </div>
+                    <div class="border rounded-xl rounded-b-none w-full">
+                        <img id='book-images' src="${url}"
+                            alt = "" class= 'mx-auto rounded-xl rounded-b-none' >
+                    </div >
 
-                <div class="p-6">
-                    <h1 class="text-3xl font-bold text-gray-800 cursor-pointer mb-4">${book.title}</h1>
-                    <h2 class="text-xl text-gray-800 font-semibold mb-4">by <span class="text-pink-400">${book.author_name == undefined || book.author_name == null ? 'Unknown Author' : book.author_name}</span></p>
+                <div class="p-6 text-left">
+                    <h1 class="text-3xl font-bold text-gray-800 cursor-pointer mb-4"><span class="text-2xl">Book Name: ${book.title}</span></h1>
+                    <h2 class="text-xl text-gray-800 font-semibold mb-4"><span class="text-pink-400">Author: ${book.author_name == undefined || book.author_name == null ? 'Unknown Author' : book.author_name[0]}</span></p>
                     <p class="text-lg font font-thin mb-4">Published By: ${book.publisher == undefined ? "Not Available" : book.publisher[0]}</p>
                     <p class="text-lg font font-semibold mb-4">First Publish Year: <span class="text-blue-400 font-semibold">${book.first_publish_year == undefined ? 'Not Available' : book.first_publish_year}</span></p>
 
-                </div>
-            </div>
-        
-            `;
+                    </div>
+                </div >
+
+    `;
             bookCardsContainer.appendChild(bookDiv);
 
         });
     }
 }
+
